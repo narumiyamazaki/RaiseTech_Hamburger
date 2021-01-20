5a4bd16750a9de02807f248086580fe65ffed50a
narumiyamazaki <flaming.june123@gmail.com>
2021-01-20 09:27:09 +0900

    コミットメッセージは必ず入れなければならないようだ

--- RaiseTech_hamburger.js
+++ RaiseTech_hamburger.js
@@ -2,15 +2,14 @@
 //tb 768px pc 1024px
 
 //メニューの開閉
-<script type="text/javascript">
-{$(window).resize(function(){
+$(window).resize(function(){
 let x = $(window).width();
 let y = 1024;
 //windowサイズがpcサイズの1024px以上になったら
 if (x >= y) {
-$('p__nav--body').addClass('p__nav--bodyopen').removeClass('p__nav--body');
+$('.p__nav--body').addClass('p__nav--bodyopen').removeClass('p__nav--body');
 }else{
-$('p__nav--body').addClass('p__nav--body').removeClass('p__nav--body');
+$('.p__nav--body').addClass('p__nav--body').removeClass('p__nav--body');
 }
-})};
-</script>
+});
+
