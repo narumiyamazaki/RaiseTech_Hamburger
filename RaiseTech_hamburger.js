4d63cacf8180e56c3779d940a8f0cf952614172e
narumiyamazaki <flaming.june123@gmail.com>
2021-02-12 19:42:40 +0900

    20210212_1942

--- RaiseTech_hamburger.js
+++ RaiseTech_hamburger.js
@@ -1,21 +1,39 @@
-/*pc以外の時はdisplay noneにしておき、PCになった時は表示させる。
-//tb 768px pc 1024px
+//windowサイズによってheader画像を切り替える。
 
-//メニューの開閉
-//メニューの開閉は、breakpoinをmixinで定義しているので、include使えばjsなくても実装できそうな気がするため、コメントアウトする
-$(window).resize(function(){
-let x = $(window).width();
-let y = 1024;
-//windowサイズがpcサイズの1024px以上になったら
-if (x >= y) {
-$('.p__nav--body').addClass('p__nav--bodyopen').removeClass('p__nav--body');
-//windowサイズがpcサイズの1024未満になったら
-}else if(x < y){
-//class名がp__nav--bodyopenの時、windwサイズが1024px未満になったらp__nav--bodyに変更する
-$('.p__nav--bodyopen').addClass('p__nav--body').removeClass('p__nav--bodyopen');
-}
-})
-else;
-});
-
-*/
+$(function() {
+    // 置換の対象とするclass属性。
+    var $elem = $('.p__header--image--js--iswitch');
+    // 置換の対象とするsrc属性の末尾の文字列。
+    var sp = '_sp';
+    var tbpc = '_tbpc';
+    // 画像を切り替えるウィンドウサイズ。
+    var replaceWidth = 768;
+  
+    function imageSwitch() {
+      // ウィンドウサイズを取得する。
+      var windowWidth = parseInt(window.innerWidth);
+  
+      // ページ内にあるすべての`.js-image-switch`に適応される。
+      $elem.each(function() {
+        var $this = $(this);
+        // ウィンドウサイズが768px以上であれば_spを_pcに置換する。
+        if(windowWidth >= replaceWidth) {
+          $this.attr('src', $this.attr('src').replace(sp, tbpc));
+  
+        // ウィンドウサイズが768px未満であれば_pcを_spに置換する。
+        } else {
+          $this.attr('src', $this.attr('src').replace(tbpc, sp));
+        }
+      });
+    }
+    imageSwitch();
+  
+    // 動的なリサイズは操作後0.2秒経ってから処理を実行する。
+    var resizeTimer;
+    $(window).on('resize', function() {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(function() {
+        imageSwitch();
+      }, 100);
+    });
+  });
