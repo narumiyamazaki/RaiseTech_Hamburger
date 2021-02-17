446f4502a70f7e458f52aacd32fffdbf1f78c00f
narumi <flaming.june123@gmail.com>
2021-02-17 22:11:52 +0900

    20210217_2211

--- RaiseTech_hamburger.js
+++ RaiseTech_hamburger.js
@@ -47,10 +47,15 @@ $(function() {
     $(".p__nav").addClass("p__nav-is-open").removeClass("p__nav");
     $(".p__sidemenu--backgroundcolor").addClass("p__sidemenu--backgroundcolor-is-active").removeClass("p__sidemenu--backgroundcolor");
     $(".p__nav--container").addClass("p__nav--container-is-open").removeClass("p__nav--container");
+    $(".l__wrapper--main").addClass("l__wrapper--main-is-hidden").removeClass("l__wrapper--main");
+    $(".l__footer").addClass("l__footer-is-hidden").removeClass("l__footer");
     });
     $(".p__nav--btn").on("click",function() {
     $(".p__nav-is-open").addClass("p__nav").removeClass("p__nav-is-open");
     $(".p__sidemenu--backgroundcolor-is-active").addClass("p__sidemenu--backgroundcolor").removeClass("p__sidemenu--backgroundcolor-is-active");
     $(".p__nav--container-is-open").addClass("p__nav--container").removeClass("p__nav--container-is-open");
+    $(".l__body-is-fixed").addClass("l__body").removeClass("l__body-is-fixed");
+    $(".l__wrapper--main-is-hidden").addClass("l__wrapper--main").removeClass("l__wrapper--main-is-hidden");
+    $(".l__footer-is-hidden").addClass("l__footer").removeClass("l__footer-is-hidden");
   });
 });
